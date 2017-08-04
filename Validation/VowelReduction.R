8dcf3f66f4b57419313d51c0fac4cd9d9ddf9a24
Rob van Son <R.J.J.H.vanSon@gmail.com>
2017-08-04 17:13:30 +0200

    Added some validation results

---
+++ Validation/VowelReduction.R
@@ -0,0 +1,14 @@
+# R
+#
+VowelTable <- read.table("VowelReductionTest.tsv", header = TRUE, sep = "\t", na.strings = "-");
+AverageVowels <- aggregate(cbind(Area, N, i.dist, u.dist, a.dist)~Speaker+Sex+Style, data=VowelTable, mean);
+SdevVowels <- aggregate(cbind(Area, N, i.dist, u.dist, a.dist)~Speaker+Sex+Style, data=VowelTable, sd);
+
+
+for (Speaker in c("N","I","G","L","E","R","K","H", "D", "O")) {
+print(t.test(VowelTable[VowelTable$Style=="VI"&VowelTable$Speaker==Speaker,]$Area, VowelTable[VowelTable$Style=="VT"&VowelTable$Speaker==Speaker,]$Area));
+};
+
+analysisVowel<-lm(Area ~ Speaker+Style, VowelTable)
+print(anova(analysisVowel))
+print(summary.lm(analysisVowel))

adb64fc791e322ff0907c330cbc47bed48704652
Rob van Son <r.j.j.h.vanson@gmail.com>
2017-08-07 10:09:48 +0200

    Updated R s cript

--- Validation/VowelReduction.R
+++ Validation/VowelReduction.R
@@ -5,10 +5,12 @@ AverageVowels <- aggregate(cbind(Area, N, i.dist, u.dist, a.dist)~Speaker+Sex+St
 SdevVowels <- aggregate(cbind(Area, N, i.dist, u.dist, a.dist)~Speaker+Sex+Style, data=VowelTable, sd);
 
 
-for (Speaker in c("N","I","G","L","E","R","K","H", "D", "O")) {
-print(t.test(VowelTable[VowelTable$Style=="VI"&VowelTable$Speaker==Speaker,]$Area, VowelTable[VowelTable$Style=="VT"&VowelTable$Speaker==Speaker,]$Area));
-};
+#for (Speaker in c("N","I","G","L","E","R","K","H", "D", "O")) {
+#print(Speaker)
+#print(t.test(VowelTable[VowelTable$Style=="VI"&VowelTable$Speaker==Speaker,]$Area, VowelTable[VowelTable$Style=="VT"&VowelTable$Speaker==Speaker,]$Area));
+#};
 
+#analysisVowel<-lm(Area ~ Speaker+Style, VowelTable, subset=VowelTable$Style=="VI"|VowelTable$Style=="VT")
 analysisVowel<-lm(Area ~ Speaker+Style, VowelTable)
 print(anova(analysisVowel))
 print(summary.lm(analysisVowel))

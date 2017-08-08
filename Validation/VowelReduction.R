43b0f98b71a8e0972391231472eb60e7c868b175
Rob van Son <r.j.j.h.vanson@gmail.com>
2017-08-08 11:02:20 +0200

    Added Fixed text chunks

--- Validation/VowelReduction.R
+++ Validation/VowelReduction.R
@@ -1,9 +1,8 @@
 # R
 #
 VowelTable <- read.table("VowelReductionTest.tsv", header = TRUE, sep = "\t", na.strings = "-");
-VowelTable$Style <- factor(VowelTable$Style, levels=c("VI", "VT", "VS", "VW", "VY"), ordered=T);
-AverageVowels <- aggregate(cbind(Area, N, i.dist, u.dist, a.dist)~Speaker+Sex+Style, data=VowelTable, mean);
-SdevVowels <- aggregate(cbind(Area, N, i.dist, u.dist, a.dist)~Speaker+Sex+Style, data=VowelTable, sd);
+AverageVowels <- aggregate(cbind(Area, N, i.dist, u.dist, a.dist)~Speaker+Sex+Style+Text, data=VowelTable, mean);
+SdevVowels <- aggregate(cbind(Area, N, i.dist, u.dist, a.dist)~Speaker+Sex+Style+Text, data=VowelTable, sd);
 
 
 #for (Speaker in c("N","I","G","L","E","R","K","H", "D", "O")) {
@@ -13,11 +12,29 @@ SdevVowels <- aggregate(cbind(Area, N, i.dist, u.dist, a.dist)~Speaker+Sex+Style
 
 #analysisVowel<-lm(Area ~ Speaker+Style, data=VowelTable, subset=VowelTable$Speaker=="VI"|VowelTable$Style=="VT")
 #analysisVowel<-lm(cbind(i.dist,u.dist,a.dist) ~ Speaker+Style, data=VowelTable)
-analysisVowel<-lm(Area ~ Speaker+Style, data=VowelTable)
-print(anova(analysisVowel))
-print(summary.lm(analysisVowel))
+#analysisVowel<-lm(Area ~ Speaker+Style, data=VowelTable, subset=VowelTable$Text=="V")
+
+print("V + F")
+analysisVowelVF<-lm(Area ~ Speaker+Style+Text, data=VowelTable)
+print(anova(analysisVowelVF))
+print(summary.lm(analysisVowelVF))
+
+print("V")
+analysisVowelV<-lm(Area ~ Speaker+Style, data=VowelTable, subset=VowelTable$Text=="V")
+print(anova(analysisVowelV))
+print(summary.lm(analysisVowelV))
+
+print("F")
+analysisVowelF<-lm(Area ~ Speaker+Style, data=VowelTable, subset=VowelTable$Text=="F")
+print(anova(analysisVowelF))
+print(summary.lm(analysisVowelF))
 
 # Box plot of averages by style
-plot(VowelTable$Style, VowelTable$Area)
+VowelTable$Style <- factor(VowelTable$Style, levels=c("I", "R", "T", "S", "W", "Y"), ordered=T);
+plot(VowelTable[VowelTable$Text=="V",]$Style, VowelTable[VowelTable$Text=="V",]$Area)
+plot(VowelTable[VowelTable$Text=="F",]$Style, VowelTable[VowelTable$Text=="F",]$Area)
 
-plot(AverageVowels$Style, AverageVowels$Area)
+plot(AverageVowels[AverageVowels$Text=="V",]$Style, AverageVowels[AverageVowels$Text=="V",]$Area)
+plot(AverageVowels[AverageVowels$Text=="F",]$Style, AverageVowels[AverageVowels$Text=="F",]$Area)
+
+plot(VowelTable$Style, VowelTable$Area)

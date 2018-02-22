28021cf57f5afd1231e002ef0e8abaf517eb3dc6
Rob van Son <r.j.j.h.vanson@gmail.com>
2018-02-22 15:07:49 +0100

    Removed old scripts

--- Validation/AverageVowels.R
+++
@@ -1,4 +0,0 @@
-VowelTable <- read.table("VowelFormantsFPA.tsv", header = TRUE, sep = "\t", na.strings = "-");
-AverageVowels <- aggregate(cbind(F1, F2)~Phon+Sex, data=VowelTable, mean);
-SdevVowels <- aggregate(cbind(F1, F2)~Phon+Sex, data=VowelTable, sd);
-
